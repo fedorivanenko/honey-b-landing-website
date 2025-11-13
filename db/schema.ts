41eb954169850f85fee9a72d3fd58500d6f2ec13
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-13 20:58:41 +0200

    add zod

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,15 @@
-import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core"
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+} from "drizzle-orm/pg-core"
 
 export const contacts = pgTable("contacts", {
   id: serial("id").primaryKey(),
+  createdAt: timestamp("created_at").defaultNow(),
   fullName: varchar("full_name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull(),
+  organization: varchar("organization", { length: 255 }),
   message: varchar("message", { length: 2000 }),
-  createdAt: timestamp("created_at").defaultNow(),
 })
---
+++ db/zod.ts
@@ -0,0 +1,26 @@
+import { createInsertSchema } from "drizzle-zod"
+import { contacts } from "@/db/schema"
+
+const emailRegex =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+export const contactFormSchema = createInsertSchema(contacts, {
+    fullName: (f) =>
+        f
+            .min(1, { message: "Required" })
+            .max(255,  { message: "Too long" }),
+
+    email: (f) =>
+        f
+            .min(1, { message: "Required" })
+            .max(255,  { message: "Too long" })
+            .regex(emailRegex, { message: "Invalid email" }),
+
+    organization: (f) =>
+        f
+            .min(1, { message: "Required" })
+            .max(255,  { message: "Too long" }),
+
+    message: (f) =>
+        f.max(2000, { message: "Too long" }),
+})
