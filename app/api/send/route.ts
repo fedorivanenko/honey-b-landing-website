3ecf7596946bd20610723c189e3c7e64b7da7165
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-13 20:24:06 +0200

    resend api update

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,23 +1,33 @@
-import { EmailTemplate } from '@/components/email/email-template';
-import { Resend } from 'resend';
+import { EmailTemplate } from "@/components/email/email-template";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+    try {
+        const sendingEmail = process.env.SENDING_EMAIL;
+        const listStr = process.env.MAILING_LIST;
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
+        if (!sendingEmail) {
+            return Response.json({ error: "SENDING_EMAIL env missing" }, { status: 500 });
+        }
+        if (!listStr) {
+            return Response.json({ error: "MAILING_LIST env missing" }, { status: 500 });
+        }
+
+        const recipients = listStr.split(",").map((e) => e.trim());
+
+        const { data, error } = await resend.emails.send({
+            from: `HoneyB alert <${sendingEmail}>`,
+            to: recipients,
+            subject: "New Inquiry",
+            react: EmailTemplate({ firstName: "John" }),
+        });
+
+        if (error) return Response.json({ error }, { status: 500 });
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
     }
-
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
 }
