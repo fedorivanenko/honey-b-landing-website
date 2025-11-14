1be990b9b2fc46fd80b06c6ca4021f17b6df61ca
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-14 19:38:54 +0200

    schema fixes

--- db/schema.ts
+++ db/schema.ts
@@ -1,15 +1,17 @@
 import {
   pgTable,
-  serial,
+  uuid,
   varchar,
   timestamp,
+  text,
 } from "drizzle-orm/pg-core"
 
 export const contacts = pgTable("contacts", {
-  id: serial("id").primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   createdAt: timestamp("created_at").defaultNow(),
   fullName: varchar("full_name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull(),
   organization: varchar("organization", { length: 255 }),
   message: varchar("message", { length: 2000 }),
+  idempotencyKey: text("idempotency_key").notNull().unique(),
 })
--- db/zod.ts
+++ db/zod.ts
@@ -8,18 +8,18 @@ export const contactFormSchema = createInsertSchema(contacts, {
     fullName: (f) =>
         f
             .min(1, { message: "Required" })
-            .max(255,  { message: "Too long" }),
+            .max(255, { message: "Too long" }),
 
     email: (f) =>
         f
             .min(1, { message: "Required" })
-            .max(255,  { message: "Too long" })
+            .max(255, { message: "Too long" })
             .regex(emailRegex, { message: "Invalid email" }),
 
     organization: (f) =>
         f
             .min(1, { message: "Required" })
-            .max(255,  { message: "Too long" }),
+            .max(255, { message: "Too long" }),
 
     message: (f) =>
         f.max(2000, { message: "Too long" }),
